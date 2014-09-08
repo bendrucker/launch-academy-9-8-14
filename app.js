c6cccf5c298920598fbfb9906dfadcabfe879b06
Ben Drucker <bvdrucker@gmail.com>
2014-09-08 12:35:07 -0400

    Reset form on submit

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var messagesRef = new Firebase('https://launch-academy-chat.firebaseio.com/messages');
 
 $(document).ready(function () {
+  var usernameField = $('#username');
+  var messageField = $('#message');
+  function cleanupForm () {
+    [usernameField, messageField]
+      .forEach(function (field) {
+        field.val('');
+      });
+  }
+
   $('form').on('submit', function (event) {
     event.preventDefault();
     messagesRef.push({
-      username: $('#username').val(),
-      message: $('#message').val()
+      username: usernameField.val(),
+      message: messageField.val()
     });
+    cleanupForm();
   });
   messagesRef.on('child_added', function (snapshot) {
     var message = snapshot.val();
